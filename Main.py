0e701b86b5c1bb68da1071c652a471be26806cb0
Gael Cruz27 <l24120349@morelia.tecnm.mx>
2024-09-12 20:06:38 -0600

    Se subio la tarea 4

---
+++ Main.py
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear objetos Coche
+        Coche coche1 = new Coche("Honda", "Civic", 2022);
+        Coche coche2 = new Coche("Chevrolet", "Camaro", 2021);
+        Coche coche3 = new Coche("Mercedes-Benz", "GLE", 2023);
+        coche1.mostrarInformacion();
+        coche2.mostrarInformacion();
+        coche3.mostrarInformacion();
+    }
+}
+
+
